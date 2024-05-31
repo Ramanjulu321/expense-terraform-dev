b481177ec0e029fc122f5f916e7c63b2445ea0dd
Ramanji <ramanjulu7890@gmail.com>
2024-05-31 10:02:26 +0530

    Dev

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -4,5 +4,5 @@ dnf install ansible -y
 cd /tmp
 git clone https://github.com/Ramanjulu321/expense-ansible-roles.git
 cd expense-ansible-roles
-ansible-playbook ansible_user=ec2-user ansible_password=DevOps321 main.yaml -e component=backend -e login_password=ExpenseApp1
-ansible-playbook ansible_user=ec2-user ansible_password=DevOps321 main.yaml -e component=frontend
+ansible-playbook -i inventory.ini main.yaml -e component=backend -e login_password=ExpenseApp1
+ansible-playbook -i inventory.ini main.yaml -e component=frontend
